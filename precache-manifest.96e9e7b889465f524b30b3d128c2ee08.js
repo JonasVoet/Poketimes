9e635f3e720dedb51e53c672b56399045e5097c0
Jonas Voetmann <48560462+JonasVoet@users.noreply.github.com>
2019-09-11 19:25:54 +0200

    f

---
+++ precache-manifest.96e9e7b889465f524b30b3d128c2ee08.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "770e3ba2055b6bce89690b9c6256669e",
+    "url": "/Poketimes/index.html"
+  },
+  {
+    "revision": "8a744bf99f850e72bdad",
+    "url": "/Poketimes/static/css/main.4dcd20e2.chunk.css"
+  },
+  {
+    "revision": "14c4db84916d8b242af9",
+    "url": "/Poketimes/static/js/2.49c70501.chunk.js"
+  },
+  {
+    "revision": "8a744bf99f850e72bdad",
+    "url": "/Poketimes/static/js/main.958087bd.chunk.js"
+  },
+  {
+    "revision": "c73cbf20b73a603b1f57",
+    "url": "/Poketimes/static/js/runtime~main.142a9056.js"
+  },
+  {
+    "revision": "25e309404b737bf64e618472c3a8480a",
+    "url": "/Poketimes/static/media/bal.25e30940.png"
+  },
+  {
+    "revision": "18032727bf8f2983658e3ce53a605417",
+    "url": "/Poketimes/static/media/char.18032727.png"
+  },
+  {
+    "revision": "d972a1ed4d90050f21d32892906e4750",
+    "url": "/Poketimes/static/media/mew.d972a1ed.png"
+  },
+  {
+    "revision": "76f027c1d8ea2170cedd976017330331",
+    "url": "/Poketimes/static/media/pika.76f027c1.png"
+  },
+  {
+    "revision": "f10bf0780aecd8ff951259976c1784dd",
+    "url": "/Poketimes/static/media/pokeball.f10bf078.png"
+  }
+]);
